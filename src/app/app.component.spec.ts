1c8bdce85e103427c3cb6ddc7f5f27280a290ae0
flaviuspilca <flavius292005@yahoo.com>
2021-03-14 18:48:17 +0100

    changes needed to deploy_2

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,6 +20,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('poll-app app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular-test app is running!');
   });
 });
